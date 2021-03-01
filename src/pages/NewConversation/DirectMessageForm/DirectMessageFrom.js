b699b7a7b0f1b4f0a7824e4f3cdb9d89eea092a0
Arkadiusz Orlowski <arkad.orlowski@gmail.com>
2021-03-01 18:00:24 +0100

    Add Redirect inside component, depending on newly created conversation, add preview if conversation exists
    -Add Redirect inside component, depending on newly created conversation
    -Check existence of DM conversation, if exists show preview, after submit redirect to created/existed conversation
    -Fix avatar, and heading display

--- src/pages/NewConversation/DirectMessageForm/DirectMessageFrom.js
+++ src/pages/NewConversation/DirectMessageForm/DirectMessageFrom.js
@@ -7,7 +7,10 @@ import SendMessageForm from "../../../components/SendMessageForm/SendMessageForm
 import ConversationBar from "../../../components/ConversationBar/ConversationBar";
 
 // Routes
-import { NEW_CONVERSATION } from "../../../consts/routes";
+import {
+  CONVERSATION_WITHOUT_ID,
+  NEW_CONVERSATION,
+} from "../../../consts/routes";
 
 // Hooks
 import useDirectMessageFrom from "./useDirectMessageForm";
@@ -18,17 +21,26 @@ import classes from "./DirectMessageForm.module.scss";
 function DirectMessageForm() {
   const {
     receivers,
+    members,
     headingString,
-    createConversationHandler,
+    submitHandler,
     messages,
     conversation,
     avatar,
   } = useDirectMessageFrom();
 
   if (conversation) {
-    return <Redirect to={`CONVERSATION_WITHOUT_ID${conversation._id}`} />;
+    return (
+      <Redirect
+        to={{
+          pathname: `${CONVERSATION_WITHOUT_ID}${conversation._id}`,
+          state: {
+            fromCreateDm: true,
+          },
+        }}
+      />
+    );
   }
-
   if (receivers.length === 0) {
     return <Redirect to={NEW_CONVERSATION} />;
   }
@@ -36,8 +48,8 @@ function DirectMessageForm() {
   return (
     <section className={classes.Section}>
       <ConversationBar headingString={headingString} avatar={avatar} />
-      <Messages messages={messages} members={receivers} />
-      <SendMessageForm submitHandler={createConversationHandler} />
+      <Messages messages={messages} members={members} />
+      <SendMessageForm submitHandler={submitHandler} />
     </section>
   );
 }
