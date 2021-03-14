6ab3679a941265e328e90b8e993aca0a78793626
Arkadiusz Orlowski <arkad.orlowski@gmail.com>
2021-03-14 13:44:49 +0100

    Export logic to custom hook, add scrolling functionalities
    Scrolling functionalities:
    * When conversation show-up, automatically scroll messages to bottom
    * When new message create or received scroll to bottom
    * When scroll to top get messages history (it's not finished, waiting for backend)

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { useTransition, animated } from "react-spring";
 
 // Components
 import Message from "./Message/Message";
@@ -7,38 +7,64 @@ import Message from "./Message/Message";
 // Styles
 import classes from "./Messages.module.scss";
 
-function Messages({ messages, members }) {
-  const { conversationId } = useParams();
-  const refMessagesEnd = useRef();
-  // const isIntersecting = useOnScreen(refMessagesEnd);
+// Hooks
+import useMessages from "./useMessages";
 
-  useEffect(() => {
-    refMessagesEnd.current.scrollIntoView();
-  }, [conversationId]);
+function Messages({ messages, members }) {
+  const { showMessages, refMessagesStart, refMessagesEnd } = useMessages(
+    messages
+  );
+  const transitions = useTransition(showMessages, null, {
+    from: {
+      width: "100%",
+      display: "grid",
+      gridTemplateColumns: "1rem 2fr 5fr 2fr 1rem",
+      alignContent: "start",
+      listStyle: "none",
+      paddingTop: "1rem",
+      position: "relative",
+      opacity: 0,
+    },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
 
   return (
-    <ul className={classes.Messages}>
-      {messages.length !== 0
-        ? messages.map((message) => {
-            return (
-              <Message
-                content={message.content}
-                senderId={message.sender}
-                avatar={
-                  members.find(
-                    (member) =>
-                      member?._id === message.sender ||
-                      member.id === message.sender
-                  ).avatar50x50
-                }
-                key={message.tempId ? message.tempId : message._id}
-                isSending={message.isSending}
-              />
-            );
-          })
-        : null}
-      <li ref={refMessagesEnd}>&nbsp;</li>
-    </ul>
+    <div className={classes.MessagesWrapper}>
+      {transitions.map(
+        ({ item, props, key }) =>
+          item && (
+            <animated.ul key={key} style={props}>
+              <li
+                ref={refMessagesStart}
+                className={classes.MessagesStartListItem}
+              >
+                &nbsp;
+              </li>
+              {messages.map((message) => {
+                return (
+                  <Message
+                    content={message.content}
+                    senderId={message.sender}
+                    avatar={
+                      members.find(
+                        (member) =>
+                          member?._id === message.sender ||
+                          member.id === message.sender
+                      ).avatar50x50
+                    }
+                    key={message.tempId ? message.tempId : message._id}
+                    isSending={message.isSending}
+                  />
+                );
+              })}
+              <li ref={refMessagesEnd} className={classes.MessagesEndListItem}>
+                &nbsp;
+              </li>
+            </animated.ul>
+          )
+      )}
+    </div>
   );
 }
 
