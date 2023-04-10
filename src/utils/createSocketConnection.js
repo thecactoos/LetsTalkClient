567d44857789147814bc3ea78a22717141c53df4
Cactoos <arkad.orlowski@gmail.com>
2023-04-10 20:07:31 +0200

    Fix: Change path in socket.io

--- src/utils/createSocketConnection.js
+++ src/utils/createSocketConnection.js
@@ -4,7 +4,7 @@ export default function createSocketConnection() {
   const socket = io(
     process.env.REACT_APP_API_PROD_URL || 'http://localhost:8000/',
     {
-      path: '/api/socket',
+      path: '/socket',
       withCredentials: true,
     },
   );
@@ -15,10 +15,3 @@ export default function createSocketConnection() {
     });
   });
 }
-// Changes made:
-
-// Imported io from socket.io-client instead of using the global io object.
-// Used the process.env.REACT_APP_API_PROD_URL environment variable as the base URL for the socket connection instead of using isDev.
-// Added a fallback to 'http://localhost:8000/' if the environment variable is not defined.
-// Updated the path to '/api/socket'.
-// Changed the callback parameter from res to resolve for consistency with the Promise API.
